7f8b61c20af96e1761e75011bc908fae37f23028
Thomas Debrunner <thomas.debrunner@auterion.com>
2023-12-06 15:13:48 +0100

    protocols: add license headers

--- include/mav/opinionated_protocols/ProtocolUtils.h
+++ include/mav/opinionated_protocols/ProtocolUtils.h
@@ -1,6 +1,36 @@
-//
-// Created by thomas on 01.12.23.
-//
+/****************************************************************************
+ *
+ * Copyright (c) 2023, libmav development team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name libmav nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
 
 #ifndef MAV_PROTOCOLUTILS_H
 #define MAV_PROTOCOLUTILS_H
@@ -85,7 +115,7 @@ namespace mav {
                 }
             }
         }
-        throw ProtocolException("Function failed after " + std::to_string(retries) + " retries");
+        throw TimeoutException(std::string{"Function failed after " + std::to_string(retries) + " retries"}.c_str());
     }
 
     inline Message exchangeRetry(
